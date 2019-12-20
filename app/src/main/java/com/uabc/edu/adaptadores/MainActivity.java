65e469b8500a7d02ccaf9274074adc9ecc362f17
xhendor <rosendorsc@gmail.com>
2019-12-19 17:27:41 -0800

    Initial commit

---
+++ app/src/main/java/com/uabc/edu/adaptadores/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uabc.edu.adaptadores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
